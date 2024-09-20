c086b1914b9e84d35d3f768a245486ea6ed3a7f4
Satisf1ed <zhogov.andrey.d@gmail.com>
2024-09-20 16:19:42 +0300

    implement inference for model

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+import math
+import tiktoken
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import math
 
 
 @dataclass
@@ -169,3 +170,33 @@ class GPT(nn.Module):
 # -----------------------------------------
 model = GPT.from_pretrained('gpt2')
 model.eval()
+
+print('model configured successfully.')
+
+max_length = 30
+num_return_sequences = 5
+
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens
+
+torch.manual_seed(42)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x)[:, -1, :]
+        probs = F.softmax(logits, dim=-1)
+
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+
+        x = torch.cat((x, xcol), dim=-1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print('>', decoded)
+
+
